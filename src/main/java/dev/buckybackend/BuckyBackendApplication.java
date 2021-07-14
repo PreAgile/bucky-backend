42ae26d77aec7f700dc1c1aeea8ea4e10d08e99b
Jieun Lee <ji.eun.lee@kt.com>
2021-07-15 00:46:17 +0900

    session 저장을 위한 Redis 설정/dependency 추가

--- src/main/java/dev/buckybackend/BuckyBackendApplication.java
+++ src/main/java/dev/buckybackend/BuckyBackendApplication.java
@@ -3,9 +3,11 @@ package dev.buckybackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisHttpSession
 public class BuckyBackendApplication {
 
     public static void main(String[] args) {
