91ed150df9928b6858bb87d4567cb44b7070e250
Jieun Lee <ji.eun.lee@kt.com>
2021-07-17 14:19:16 +0900

    스튜디오 정보 Dto 객체 생성 시 Default 생성자가 없어 발생하는 버그 수정

--- src/main/java/dev/buckybackend/dto/PhoneListDto.java
+++ src/main/java/dev/buckybackend/dto/PhoneListDto.java
@@ -2,11 +2,13 @@ package dev.buckybackend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PhoneListDto {
     @NotEmpty
     private String phone;
